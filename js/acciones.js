82d0912885222e8bd8cfb73fe9cd4b71921198be
karla-rosales <karla.dsaupoo.cb16@gmail.com>
2016-05-11 09:11:36 -0500

    subida8a

--- js/acciones.js
+++ js/acciones.js
@@ -5,22 +5,22 @@ $(document).ready(function(e) {
 		
 		$('#izquierda').on("swipeleft",function(){
 			navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion8","Aceptar"});
-		});
-		
-		$('#derecha').on("swiperight",function(){
-			navigator.notification. confirm("¿que quieres hacer?",function(opt){
-				switch(opt)
-				{
-					case 1:
-					navigator.notification.beep(1);
-					break;
-					
-					case 2:
-					navigator.notification.vibrate(1000);
-					break;
-				}
-			},"Aplicacion8","Beep,Vibrar,Cancelar");
-		});
-	},false);
 });
 
+$('#derecha').on("swiperight",function(){
+	navigator.notification. confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"Aplicacion8","Beep,vibrar,Cancelar");
+	
+});
+	},false);
+});
